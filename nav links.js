8d255c314add7aca69fc9c471cf034b7c21c82f6
ChiaJunJie <47118038+ChiaJunJie@users.noreply.github.com>
2019-06-25 22:25:27 +0800

    Update nav links.js

--- nav links.js
+++ nav links.js
@@ -2,7 +2,7 @@ var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a
 
 var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='misc.html'>Misc.</a>";
 
-if (location.href.replace(/(.+\w\/)(.+)/,"/$2") == "/index.html" || location.href == "") {
+if (location.pathname == "/index.html" || location.pathname == "/") {
 	document.getElementById('indexpage').innerHTML += indexpage;
 } else {
 	document.getElementById('sidebar').innerHTML += side;
