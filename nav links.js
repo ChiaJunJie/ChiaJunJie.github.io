0ef74999f34dee0485d6a3a5019621af409ffafc
ChiaJunJie <47118038+ChiaJunJie@users.noreply.github.com>
2019-06-25 22:07:40 +0800

    Major Overhaul™
    1. Added profile picture and information into the About page
    2. Updated navbars so they use JavaScript

---
+++ nav links.js
@@ -0,0 +1,9 @@
+var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a><a href='misc.html'>Misc.</a></nav>";
+
+var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='misc.html'>Misc.</a>";
+
+if (location.href.replace(/(.+\w\/)(.+)/,"/$2") == "/index.html") {
+	document.getElementById('indexpage').innerHTML += indexpage;
+} else {
+	document.getElementById('sidebar').innerHTML += side;
+} 
