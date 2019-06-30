d42299c9a9310ff0c74322320afdcfba138760f7
ChiaJunJie <47118038+ChiaJunJie@users.noreply.github.com>
2019-07-01 01:33:20 +0800

    updated content

--- nav links.js
+++ nav links.js
@@ -1,6 +1,6 @@
-var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a><a href='misc.html'>Misc.</a></nav>";
+var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a></nav>";
 
-var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='misc.html'>Misc.</a>";
+var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a>";
 
 if (location.pathname == "/index.html" || location.pathname == "/") {
 	document.getElementById('indexpage').innerHTML += indexpage;
