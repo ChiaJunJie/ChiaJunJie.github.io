3a76fe60f4a1941f8101929efdb804611e4731fc
ChiaJunJie <47118038+ChiaJunJie@users.noreply.github.com>
2019-07-12 16:06:38 +0800

    content aqdded to illustrations.jpg

--- nav links.js
+++ nav links.js
@@ -1,6 +1,6 @@
-var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a></nav>";
+var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a href='illustrations.html'>Illustrations/Imaging</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a></nav>";
 
-var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a>";
+var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustrations/Imaging</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a>";
 
 if (document.getElementById('indexpage')) {
 	document.getElementById('indexpage').innerHTML = indexpage;
