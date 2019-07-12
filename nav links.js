9e9e1f8507bbfbd2af00de159ae8975cca3d93bc
ChiaJunJie <47118038+ChiaJunJie@users.noreply.github.com>
2019-07-12 08:02:13 +0800

    edited nav links.js and style.css
    removed dependency on the end of the link for javascript
    added base for responsive design in style

--- nav links.js
+++ nav links.js
@@ -2,8 +2,9 @@ var indexpage = "<a href='aboutme.html' class='about'>About</a><nav id='main'><a
 
 var side = "<a href='index.html'>Home</a><a href='aboutme.html'>About</a><a href='illustrations.html'>Illustration</a><a href='code.html'>Coding</a><a href='writing.html'>Writing</a><a href='digitalcreations.html'>Digital Content</a>";
 
-if (location.pathname == "/index.html" || location.pathname == "/") {
-	document.getElementById('indexpage').innerHTML += indexpage;
-} else {
-	document.getElementById('sidebar').innerHTML += side;
+if (document.getElementById('indexpage')) {
+	document.getElementById('indexpage').innerHTML = indexpage;
+} 
+if (document.getElementById('sidebar')) {
+	document.getElementById('sidebar').innerHTML = side;
 } 
